03bca79fe390b5e23fbb860d3d2a3d08dada139c
Pavol Mráz <pavol.mraz@fores.group>
2020-01-27 16:50:24 +0100

    Added single-target LibANetstandard20 dependency to LibB - NuGet pack still not including the referenced project outputs.

---
+++ LibANetstandard20/LibAService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LibANetstandard20
+{
+  public class LibAService
+  {
+    public string SayHello() => $"Hello from {this.GetType().Assembly.FullName}";
+  }
+}
--- LibB/LibBService.cs
+++ LibB/LibBService.cs
@@ -1,17 +1,21 @@
-﻿using LibA;
-using System;
+﻿using System;
 
 namespace LibB
 {
   public class LibBService
   {
-    private readonly LibAService _libAService;
+    private readonly LibA.LibAService _libAService;
+    private readonly LibANetstandard20.LibAService _libANetstandard20Service;
 
     public LibBService()
     {
       this._libAService = new LibA.LibAService();
+      this._libANetstandard20Service = new LibANetstandard20.LibAService();
     }
 
-    public string SayHello() => $"Hello from {this.GetType().Assembly.FullName} and also {this._libAService.SayHello()}";
+    public string SayHello() => 
+      $"Hello from {this.GetType().Assembly.FullName} and also " + 
+      $"{this._libAService.SayHello()} and also " +
+      $"{this._libANetstandard20Service.SayHello()}";
   }
 }
