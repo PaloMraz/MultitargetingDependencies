c7975a8bd74c23e93da88526b68b60c82952fa37
Pavol Mráz <pavol.mraz@fores.group>
2020-01-27 15:25:41 +0100

    Add project files.

---
+++ LibA/LibAService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LibA
+{
+  public class LibAService
+  {
+    public string SayHello() => $"Hello from {this.GetType().Assembly.FullName}";
+  }
+}
---
+++ LibB/LibBService.cs
@@ -0,0 +1,17 @@
+﻿using LibA;
+using System;
+
+namespace LibB
+{
+  public class LibBService
+  {
+    private readonly LibAService _libAService;
+
+    public LibBService()
+    {
+      this._libAService = new LibA.LibAService();
+    }
+
+    public string SayHello() => $"Hello from {this.GetType().Assembly.FullName} and also {this._libAService.SayHello()}";
+  }
+}
